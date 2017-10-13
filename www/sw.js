e4a59b1f20a08c20a0cea37a1ea06912a2f454a5
Paul Grandperrin <paul.grandperrin@gmail.com>
2017-10-12 21:13:41 -0700

    Minor work on the PWA side off the app

--- www/sw.js
+++ www/sw.js
@@ -1,5 +1,6 @@
-var CACHE_NAME = 'buddhabrot-v1';
+var CACHE_NAME = 'buddhabrot-2017-10-12-1';
 var urlsToCache = [
+  '.',
   '/',
   '/main.js',
   '/material-components-web.min.css',
@@ -10,7 +11,8 @@ var urlsToCache = [
   '/rustybrot.wasm.js',
   '/worker-compositor.js',
   '/worker-producer.js',
-  '/manifest.json'
+  '/manifest.json',
+  '/icon.png'
 ];
 
 self.addEventListener('install', function(event) {
@@ -32,34 +34,22 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
+        return fetch(event.request);
+      }
+    )
+  );
+});
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
+this.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['buddhabrot-2017-10-12-1'];
 
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
